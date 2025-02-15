b6f49413e1edc722f19e265250b43e58eac3d822
BobKimani <Bob.Kimani@strathmore.edu>
2025-02-15 15:30:14 +0300

    Adds auth pages

---
+++ app/src/main/java/com/example/gradgoods/screens/SignIn.kt
@@ -0,0 +1,130 @@
+package com.example.gradgoods.ui.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import com.example.gradgoods.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+
+@Composable
+fun SignInScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF5E5F3))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(top= 100.dp))
+
+        Text(
+            "Welcome Back",
+            fontSize = 30.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+        Spacer (modifier = Modifier.height(30.dp))
+
+        Text(
+            "Sign in with your email or password",
+            fontSize = 20.sp,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp)
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp)
+        )
+
+        Spacer(modifier= Modifier.height(50.dp))
+
+        Button(
+            onClick = { /* Handle sign-in */ },
+            modifier = Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+        ) {
+            Text("Continue")
+        }
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            "-----Or Sign In with------",
+            fontSize = 16.sp,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.google), // Ensure this drawable is available
+            contentDescription = "Sign in with Google",
+            modifier = Modifier.size(48.dp)
+        )
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Row{
+            Text(
+                "Dont have an account? ",
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+            Text(
+                "Sign Up",
+                fontSize = 16.sp,
+                color = Color(0xFFA020F0),
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.clickable {/*Handles click */ }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInPreview() {
+    SignInScreen()
+}
---
+++ app/src/main/java/com/example/gradgoods/screens/SignUp.kt
@@ -0,0 +1,123 @@
+package com.example.gradgoods.ui.auth
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import com.example.gradgoods.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SignUpScreen() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0xFFF5E5F3))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.padding(top= 100.dp))
+
+        Text(
+            "Register Account",
+            fontSize = 30.sp,
+            color = Color.Black,
+            fontWeight = FontWeight.ExtraBold
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Text(
+            "Sign up with your email and password",
+            fontSize = 20.sp,
+            color = Color.Black
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp)
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp)
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            label = { Text("Confirm Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(24.dp)
+        )
+
+        Spacer(modifier= Modifier.height(50.dp))
+
+        Button(
+            onClick = { /* Handle sign-up */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            shape = RoundedCornerShape(12.dp)
+        ) {
+            Text("Continue")
+        }
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Row {
+            Text(
+                "Already have an account? ",
+                fontSize = 16.sp,
+                color = Color.Black
+            )
+            Text(
+                "Sign In",
+                fontSize = 16.sp,
+                color = Color(0xFFA020F0),
+                textDecoration = TextDecoration.Underline,
+                modifier = Modifier.clickable { /* Handle navigation to Sign In */ }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpPreview() {
+    SignUpScreen()
+}
